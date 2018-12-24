3f7817ee263a4ee950433fa2b956195f2c3945f2
Robin Wilson <robinwilson@rwsbetas.com>
2018-12-24 01:22:49 +0000

    First version

---
+++ TheComfortZoneSalon/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace TheComfortZoneSalon
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Build().Run();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:5007")
+                //.UseSetting("https_port", "5007")
+                .ConfigureKestrel((context, options) =>
+                {
+                    // Set properties and call methods on options
+                });
+    }
+}

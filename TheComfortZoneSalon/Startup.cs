2d6c7fe1282d04be63a4b74835432b79c0f9e4c1
Robin Wilson <robinwilson@rwsbetas.com>
2020-01-01 12:43:36 +0000

    Moved to .NET Core 3.1 + Update Plugins

--- TheComfortZoneSalon/Startup.cs
+++ TheComfortZoneSalon/Startup.cs
@@ -6,9 +6,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using TheComfortZoneSalon.Data;
 using Microsoft.Extensions.Configuration;
@@ -29,21 +27,11 @@ namespace TheComfortZoneSalon
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>()
-                .AddDefaultUI()
+            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
-
-
             services.AddRazorPages();
         }
 
@@ -62,23 +50,14 @@ namespace TheComfortZoneSalon
                 app.UseHsts();
             }
 
-            //Avoid CORS errors with JS files served over CDN
-            app.UseCors(builder => builder.WithOrigins(
-                "http://www.thecomfortzonesalon.co.uk",
-                "https://www.thecomfortzonesalon.co.uk",
-                "http://thecomfortzonesalon.co.uk",
-                "https://thecomfortzonesalon.co.uk"
-                )
-                .AllowAnyMethod()
-                .AllowAnyHeader()
-            );
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
+
             app.UseAuthentication();
             app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
